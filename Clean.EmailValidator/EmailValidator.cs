27e79626440b5e3d15307d1cb6a232b90efb65e0
Barthmosz <126964783+Barthmosz@users.noreply.github.com>
2023-05-01 23:52:57 -0300

    feat: ensure EmailValidator validates properly

---
+++ Clean.EmailValidator/EmailValidator.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Clean.EmailValidator
+{
+    public static class EmailValidator
+    {
+        public static bool Validate(string email)
+        {
+            string emailRegex = "/^[\\w-.]+@([\\w-]+\\.)+[\\w-]{2,4}$/g";
+            return Regex.IsMatch(email, emailRegex);
+        }
+    }
+}
