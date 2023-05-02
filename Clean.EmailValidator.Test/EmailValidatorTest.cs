33085b91522e0205d19becb59c4ac17bb8b6f426
Barthmosz <126964783+Barthmosz@users.noreply.github.com>
2023-05-02 00:49:14 -0300

    test: ensure EmailValidator returns false if an invalid email is provided

---
+++ Clean.EmailValidator.Test/EmailValidatorTest.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace Clean.EmailValidator.Test
+{
+    [TestFixture]
+    public class EmailValidatorTest
+    {
+        [Test, Description("ensure EmailValidator returns false if an invalid email is provided.")]
+        public void EmailValidator_InvalidEmail_ReturnsFalse()
+        {
+            Assert.That(EmailValidator.Validate("@mail.com"), Is.EqualTo(false));
+        }
+    }
+}
