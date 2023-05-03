b155358ba49654b737e166f91baad0e2e2b52d42
Barthmosz <126964783+Barthmosz@users.noreply.github.com>
2023-05-02 21:57:28 -0300

    docs: add docstring to EmailValidator

--- Clean.EmailValidator/EmailValidator.cs
+++ Clean.EmailValidator/EmailValidator.cs
@@ -4,6 +4,11 @@ namespace Clean.EmailValidator
 {
     public static class EmailValidator
     {
+        /// <summary>
+        /// Validates an email address.
+        /// </summary>
+        /// <param name="email"></param>
+        /// <returns></returns>
         public static bool Validate(string email)
         {
             string emailRegex = "^[\\w-.]+@([\\w-]+\\.)+[\\w-]{2,4}$";
